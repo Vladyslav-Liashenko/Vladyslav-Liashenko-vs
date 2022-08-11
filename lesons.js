aeb51479d8eec17d62b6f3eb499dd586399b97f3
VladLiashenko <lyashenko462@gmail.com>
2022-08-11 21:11:35 +0300

    auto

---
+++ lesons.js
@@ -0,0 +1,64 @@
+
+
+function END (event) {
+    document.getElementById("textDisplay").value;
+}
+
+
+
+
+function LeftS () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + ')';
+}
+function RightS () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '(';
+}
+function Delym () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '/';
+}
+function Ymnoj () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '*';
+}
+function Minus () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '-';
+}
+function Plus () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '+';
+}
+
+
+function numberCe () {
+    document.getElementById("textDisplay").value= null;
+}
+
+
+function number1 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '1';
+}
+function number2 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '2';
+}
+function number3 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '3';
+}
+function number4 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '4';
+}
+function number5 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '5';
+}
+function number6 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '6';
+}
+function number7 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '7';
+}
+function number8 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '8';
+}
+function number9 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '9';
+}
+function number0 () {
+    document.getElementById("textDisplay").value= document.getElementById("textDisplay").value + '0';
+}

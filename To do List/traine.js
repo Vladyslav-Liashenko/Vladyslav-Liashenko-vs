fc429c7acd95534f4cdf929db89aa1886083f77f
VladLiashenko <lyashenko462@gmail.com>
2022-08-30 13:55:31 +0300

    ррр

--- To do List/traine.js
+++ To do List/traine.js
@@ -1,23 +1,7 @@
 const inputTaskName = document.querySelector("input[type='text']");
-const inputTaskState = document.querySelector("select[class='btn btn-light']");
-const ol = document.querySelector("ol.taskValue");
-let toDo = [
-    {
-        "nameTask": "dfsdf",
-        "state": "active",
-        "id": 1660830161982
-    },
-    {
-        "nameTask": "dfsdsf",
-        "state": "active",
-        "id": 1660830164699
-    },
-    {
-        "nameTask": "dsfd",
-        "state": "active",
-        "id": 1660830166999
-    }
-];
+var ol = document.querySelector("ol.taskValue");
+let toDo = [];
+var statys = ['Active', 'Completed', 'Deleted'];
 
 
 function addTask () {
@@ -25,65 +9,63 @@ function addTask () {
          // Создаем обьект
         let taskObject = {
             nameTask: inputTaskName.value,
-            state: inputTaskState.value,
             id: new Date().valueOf(),
         };
                                
-        // добавляем в начало массива [toDo] содержимое (addTask)
+        // добавляем в конец массива [toDo] содержимое (addTask)
+            // toDo.shift(taskObject);
             toDo.push(taskObject);
             console.log(toDo);
             inputTaskName.value = '';
+
+            //Вывод массива путем перебора элементов
+                let outTodo = document.querySelector('.taskValue');
+
+                // for ( let i = 0; i < toDo.length; i++ ) {
+                        let task = toDo[toDo.length - 1];
+                        let li = document.createElement('li');
+                        let span = document.createElement('span');
+                        span.classList.add('ontaskText');
+                        span.innerText = task.nameTask;
+                        let select = document.createElement('select');
+                        select.classList = ("btn btn-light");
+                        select.id = ("select");
+                        select.onchange="send(this)";
+
+                        for (const val of statys) {
+                            var option = document.createElement('option');
+                            option.value = val;
+                            option.text = val.charAt(0).toUpperCase() + val.slice(1);
+                            select.appendChild(option);
+                        };
+
+                    ol.appendChild(li);
+                    li.appendChild(span);
+                    span.appendChild(select);
+                    outTodo.appendChild(li);
+                // };
+    
     } else {
         alert('Fill in the task fields')
     }
-}
+};
 
-//Вывод массива путем перебора элементов
-let outTodo = document.querySelector('.taskValue');
 
-// let str = '';
-for ( let i = 0; i < toDo.length; i++ ) {
-    // if (toDo[i] !== undefined) str += i+toDo[i] + '<br>' ;
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    span.classList.add('ontaskText');
-    span.innerText = toDo[i].nameTask;
-    let select = document.createElement('select');
-    // select.classList.add('btn btn-light')
-    li.appendChild(span);
-    outTodo.appendChild(li);
+// Task Complate
+/* <select onchange="send(this)"></select> */
+// select.onchange = "send(this)"; // Всунуть в свойства SELECT
+
+let stateTask = document.getElementById("select");
+console.log(stateTask);
 
 
 
-
-    // <li><span class="ontaskText">Hello amigos</span><span>
-    //                 <select class="btn btn-light">
-    //                     <option id="option" value="active">Active</option>
-    //                     <option id="option" value="completed">Completed</option>
-    //                     <option id="option" value="deleted">Deleted</option>
-    //                 </select>
-    //             </span></li>
-}
-outTodo.innerHTML= str;
-
-//  function addTask () {
-//     let li = document.createElement('li');
-
-//     let taskValue = document.createElement('span');
-//     let taskCheck = document.createElement('select');
-//         li.innerHTML = inputTaskName.value;
-//         li.appendChild(taskValue, taskCheck);
-//         ol.appendChild(li);
-
-//         if (inputTaskName.value == inputTaskName.value) {
-//             inputTaskName.value = '';
-//         }
-
-//  }
-    // let taskValue = {
-
-    //     span : "Create new todo list",
-    // };
-
-    //     id: new Date().valueOf(), title : "Create new todo list", Option : "active", 
-    // };
+// function stateTask() {
+//     if (option.value == 'Active') {
+//         console.log('pause');
+//     } else if (statys[i] == 'Completed') {
+//         console.log('true');
+//     } else {
+//         console.log('fals');
+//     }
+//  };

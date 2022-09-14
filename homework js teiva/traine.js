4f2540dbb4f4a16c7b305fe4f25e55d884f96bec
VladLiashenko <lyashenko462@gmail.com>
2022-09-14 15:58:17 +0300

    save

--- homework js teiva/traine.js
+++ homework js teiva/traine.js
@@ -1,9 +1,7 @@
 //1. Using the if..else construct, write code that receives a number via prompt and then prints it to alert:
-
 // 1 if the value is greater than zero,
 // -1 if the value is less than zero,
 // 0 if the value is zero.
-
 // The user is expected to enter only numbers.
 
 var numflat = prompt("Your appartament number?", );
@@ -18,17 +16,29 @@ if ( isNaN(Number(numflat)) ) {
   alert ('Your apartment number cannot be < 0');
 };
 
-// // // 2. Write an if condition to check that the value of the age variable is NOT between 14 and 90 inclusive.
+
+// 2. Write an if condition to check that the value of the age variable is NOT between 14 and 90 inclusive.
 
 var age = prompt("Your age?", );
 
-if ( age >= 14 && age <= 90) {
-  alert('Age between 14 and 90 inclusive');
-} else if (isNaN(Number(age))){
-  alert('Write the age number. NUMBER!!!');
-} else {
-  alert('Age NOT between 14 and 90 inclusive');
-};
+let message = 
+  isNaN(Number(age)) ? "Write the age number. NUMBER!!!"
+  : age >= 14 && age <= 90 ? "Age between 14 and 90 inclusive"
+  : "Age NOT between 14 and 90 inclusive";
+alert(message);
+
+// ----------- V2 ---------------------------------------------
+// var age = prompt("Your age?", );
+
+// if (isNaN(Number(age))) {
+//   alert('Write the age number. NUMBER!!!');
+// } else if ( age >= 14 && age <= 90 ){
+//   alert('Age between 14 and 90 inclusive');
+// } else {
+//   alert('Age NOT between 14 and 90 inclusive');
+// };
+// ------------------------------------------------------------
+
 
 // 3. Use the for loop to output even numbers from 2 to 10.
 
@@ -38,27 +48,28 @@ for ( let i = 2; i < numbers.length; i += 2) {
   console.log(numbers[i]);
 };
 
+
 // 4. Write a loop that prompts the user to enter a number greater than 100. If the visitor entered a different number, ask them to enter it again, and so on.
 
-repit();
-function repit() {
+var numbers100500;
+do {
+  numbers100500 = prompt("Write number greater than 100", );
+} while ( numbers100500 <= 100 || isNaN(Number(numbers100500)));
 
-var numbers100 = prompt("Write number greater than 100", );
+// ----------- V2 ---------------------------------------------
+// repit();
+// function repit() {
 
-  if ( isNaN(Number(numbers100)) ) {
-    alert('Write number. NUMBER!!!');
-    repit();
-  } else if ( numbers100 <= 100 && numbers100 !== null) {
-    alert('Write number greater than 100');
-    repit();
-  } else if ( numbers100 > 100) {   /////////// COMPLETED!!!!
-    alert('You are smart 😂');          
-  } 
-}
+// var numbers100 = prompt("Write number greater than 100", );
 
-// ----------- V2 ----------------------------------------------
-// var numbers100500;
-// do {
-//   numbers100500 = prompt("Write number greater than 100", );
-// } while ( numbers100500 <= 100 || isNaN(Number(numbers100500)));
-// -------------------------------------------------------------
+//   if ( isNaN(Number(numbers100)) ) {
+//     alert('Write number. NUMBER!!!');
+//     repit();
+//   } else if ( numbers100 <= 100 && numbers100 !== null) {
+//     alert('Write number greater than 100');
+//     repit();
+//   } else if ( numbers100 > 100) {   /////////// COMPLETED!!!!
+//     alert('You are smart 😂');          
+//   } 
+// }
+// ------------------------------------------------------------

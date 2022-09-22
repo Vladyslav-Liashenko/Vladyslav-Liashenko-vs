1b3557bc0b6dddd9cff28acf2a9aaf41657591ab
VladLiashenko <lyashenko462@gmail.com>
2022-09-22 11:43:50 +0300

    save

--- homework js teiva/HW2/traine1.js
+++ homework js teiva/HW2/traine1.js
@@ -1,39 +1,46 @@
 // 1. Write a function, which finds the most frequently occurring element in an array.
-var arr = [1, 2, 2, 3, 1, 1, 1];
-   var counts = {};
-  
-for (var i in arr) {
+var arr = [3, 1, 1, 2, 1, 2, 4];
+  var counts = {};
+  var countV = 0;
+  var countI = 0;
+
+for (let i = 0; i < arr.length; i++) {
     counts[arr[i]] = (counts[arr[i]] || 0) + 1;
 }
-console.log(counts);
+for (let max in counts){
+  if (counts[max] > countV) {
+    countV = counts[max];
+    countI = max;
+  }
+}
+console.log("Key: " + countI + ". Max num of repetitions: " + countV + ".");
 
 // 2. Write a function, which will identify palindrome words (input-strings). (input 'dog', function returns false; input 'tenet' , function returns true;)
 
-// ('rotator'); // true ('rotator' -> 'rotator')
-// ('home'); // false ('home' -> 'emoh')
-// ('Racecar'); // true (регистр игнорируется)
-
-var target = 'Racecar';
-palindrome();
+var target = 'ono';
 
 function palindrome() {
-  var result = '';
-  var result1 = '';
-  var result2 = '';
+  return target == target.split("").reverse().join("");
+// ----------------------- V2 --------------------------
+//   var result = '';
+//   var result1 = '';
+//   var result2 = '';
 
-  for (let word of target) {
+//   for (let word of target) {
 
-    result1 = word.toLocaleUpperCase().trim().replace('') + result1;
+//     result1 = word.toLocaleUpperCase().trim().replace('') + result1;
 
-    result2 = result2 + word.toLocaleUpperCase().trim().replace('');
-  }
-  if (result1 === result2) {
-      result = true;
-    } else {
-      result = false;
-    }
-console.log(result);
+//     result2 = result2 + word.toLocaleUpperCase().trim().replace('');
+//   }
+//   if (result1 === result2) {
+//       result = true;
+//     } else {
+//       result = false;
+//     }
+// console.log(result);
+// ----------------------------------------------------------
 }
+console.log(palindrome(target))
 palindrome();
 
 // 3. Write a function, which will convert temperature from Fahrenheit to Celsius.(input [5, 15, 38], function returns [-15, -9.4, 3.3])
